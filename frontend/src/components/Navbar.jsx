fe13a31805ae23568195de3789520bed7e155371
Mohammed Junaid <junaid7mohamed@gmail.com>
2024-03-12 16:23:49 +0530

    added navbar to project

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+
+import logo from "./assets/logo.png";
+import cart_logo from "./assets/cart_icon.png"
+
+
+const Navbar = () => {
+
+  const [activeMenu,setActiveMenu] = useState("shop")
+
+  
+  return (
+    <div className='flex justify-around p-4 shadow'>
+      <div className='flex items-center gap-2'>
+        <img src={logo} alt='logo-img'/>
+        <p className='text-logo text-3xl font-semibold'>Shoppers Shop</p>
+      </div>
+    
+        <ul className='text-nav-items flex items-center gap-12 text-xl font-medium'>
+            <li onClick={()=>setActiveMenu("shop")} className='cursor-pointer'>Shop {activeMenu==="shop"?<hr className='h-1 rounded-xl bg-[#FF4141]'/>:<></>} </li>
+            <li  onClick={()=>setActiveMenu("men")}  className='cursor-pointer' >Men {activeMenu==="men"?<hr className='h-1 rounded-xl bg-[#FF4141]'/>:<></>} </li>
+            <li  onClick={()=>setActiveMenu("women")} className='cursor-pointer'>Women {activeMenu==="women"?<hr className='h-1 rounded-xl bg-[#FF4141]'/>:<></>} </li>
+            <li  onClick={()=>setActiveMenu("kids")} className='cursor-pointer'>Kids {activeMenu==="kids"?<hr className=' h-1 rounded-xl bg-[#FF4141]'/>:<></>} </li>
+        </ul>
+     
+      <div className='flex items-center gap-11'>
+        <button className='border-solid border-2 border-border rounded-full w-32 h-14  '>Login</button>
+        <img src={cart_logo} alt='cart-icon-img' />
+        <div className='bg-[#f83636] text-[#FFFFFF] w-6 h-6 flex justify-center items-center mt-[-35px] ml-[-55px] rounded-lg text-sm'>0</div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors:{
+      'logo':'#171717',
+      'nav-items':'#626262',
+      'border':'#7a7a7a'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
